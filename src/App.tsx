5f202d9f72bacf5b5fffe7cdd07bcfd8f4af84b7
juzQrios <dj30c.1@gmail.com>
2020-09-10 00:47:22 +0530

    Remove unused modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import {
   IonApp,
-  IonIcon,
-  IonLabel,
   IonRouterOutlet,
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
